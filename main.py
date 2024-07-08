acd904c953de57c414323d9e728fe1be68b36bc9
FrancescoCesari97 <freedus97@gmail.com>
2024-07-08 10:32:23 +0200

    added the stars in the display in a random position

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pygame
+from os.path import join
+from random import randint
 
 # * general setup
 pygame.init()
@@ -10,8 +12,17 @@ WINDOW_HEIGHT = 720
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 running = True
 
-# * surface
-surface = pygame.Surface((100, 200))
+# * plain surface
+# surface = pygame.Surface((100, 200))
+
+# * importing spaceship
+path = join('images', 'player.png')
+player = pygame.image.load(path).convert_alpha()
+
+# * importing star
+path = join('images', 'star.png')
+star = pygame.image.load(path).convert_alpha()
+star_position = [(randint(0, WINDOW_WIDTH), randint(0, WINDOW_HEIGHT)) for i in range(20)]
 
 while running:
     # * event loop
@@ -21,8 +32,13 @@ while running:
 
     # * draw the game 
 
-    display_surface.fill('darkgray')
-    display_surface.blit(surface, (100,200))
+    display_surface.fill('black')
+
+    for i in star_position:
+        display_surface.blit(star, i)
+
+    display_surface.blit(player, (100,200))
+
     pygame.display.update()
 
 
