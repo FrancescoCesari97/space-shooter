0af192b3e09b56aebde84e942436ad62dd11f9ec
FrancescoCesari97 <freedus97@gmail.com>
2024-07-09 11:37:57 +0200

    added player movement with delta time to match different frame rate

--- main.py
+++ main.py
@@ -12,6 +12,8 @@ WINDOW_HEIGHT = 720
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 running = True
 
+clock = pygame.time.Clock()
+
 # * plain surface
 # surface = pygame.Surface((100, 200))
 
@@ -19,6 +21,8 @@ running = True
 path = join('images', 'player.png')
 player = pygame.image.load(path).convert_alpha()
 player_rect = player.get_frect(center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
+player_direction = pygame.math.Vector2(2, -1)
+player_spped = 100
 
 # * importing star
 path = join('images', 'star.png')
@@ -40,6 +44,9 @@ laser_rect = laser.get_frect(center = (WINDOW_WIDTH / 3, WINDOW_HEIGHT / 3))
 
 
 while running:
+    # * framerate
+    dt = clock.tick() / 1000
+    print(dt)
     # * event loop
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -51,11 +58,15 @@ while running:
     for i in star_position:
         display_surface.blit(star, i)
 
-
     display_surface.blit(meteor, i)
 
-    display_surface.blit(player, player_rect)
     display_surface.blit(laser, laser_rect )
+
+
+    # * player movement
+    player_rect.center += player_direction * player_spped * dt
+    display_surface.blit(player, player_rect)
+
 
     pygame.display.update()
 
