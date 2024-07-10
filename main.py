5aa78258b8fc8cdc393c718331f8d6102ac62f49
FrancescoCesari97 <freedus97@gmail.com>
2024-07-10 15:56:24 +0200

    added meteor collision with player and laser

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ class Player(pygame.sprite.Sprite):
         
         recent_keys = pygame.key.get_just_pressed()
         if recent_keys[pygame.K_SPACE] and self.can_shoot:
-            Laser(laser_surf, self.rect.midtop, all_sprites)
+            Laser(laser_surf, self.rect.midtop, (all_sprites, laser_sprites))
             self.can_shoot = False
             self.laser_shoot_time = pygame.time.get_ticks()
         
@@ -73,6 +73,17 @@ class Meteor(pygame.sprite.Sprite):
             self.kill()
 
 
+def collisions():
+    collided_meteor = pygame.sprite.spritecollide(player, meteor_sprites, False)
+    if collided_meteor:
+        running = False
+
+    for laser in laser_sprites:
+        collided_lasers = pygame.sprite.spritecollide(laser, meteor_sprites, True)
+        if collided_lasers:
+            laser.kill()
+
+        
 # * general setup
 pygame.init()
 pygame.display.set_caption('Space Shooter')
@@ -95,10 +106,16 @@ laser_surf = pygame.image.load(join('images', 'laser.png')).convert_alpha()
 
 # * sprites
 all_sprites = pygame.sprite.Group()
+meteor_sprites = pygame.sprite.Group()
+laser_sprites = pygame.sprite.Group()
+
+
 for i in range(20):
     Star(all_sprites, star_surf)
 
 player = Player(all_sprites)
+
+
 
 
 # * meteor event
@@ -116,15 +133,19 @@ while running:
             running = False
         if event.type == meteor_event:
             x, y = randint(0, WINDOW_WIDTH), 0
-            Meteor(meteor_surf, (x, y), all_sprites)
+            Meteor(meteor_surf, (x, y), (all_sprites, meteor_sprites))
 
     # * update
     all_sprites.update(dt)
+    
+    collisions()
 
     # * draw the game 
     display_surface.fill('black')
 
     all_sprites.draw(display_surface)
+
+
 
     pygame.display.update()
 
